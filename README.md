fe19865f49703ed14c8808e768c437dd1667b572
Alireza <seyed.alireza.miryazdi@gmail.com>
2022-06-29 17:56:54 +0430

    initial commit with dynamic date/weather info

--- README.md
+++ README.md
@@ -1,16 +1,32 @@
+Hi there, I'm Alireza :wave:
+===========================
+<center>
 
+![Date](images/date.png)![](images/transition.png)![Weather](images/weather.png)  
+</center>
+<center> 
+    <img src="https://visitor-badge.laobi.icu/badge?page_id=Xosrov"> 
+</center>
+ 
+I'm currently an Engineering student, and my love for programming has helped me explore various fields.
 
-<!--
-**Xosrov/Xosrov** is a ✨ _special_ ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+## About me  
+- 🌱 I love music, nature and fictional stories. 
+- 🔭 Interested in various topics such as __web scraping__, __WebRTC__ & __livestreaming__ and automation using python.
+- 🤔 Plan to learn __Rust__ soon  
+  
+![Void Linux](images/void.png) Join the [Void](https://voidlinux.org/)
 
-Here are some ideas to get you started:
+## Technologies
+![C++](images/cpp.svg)
+![C](images/c.svg)
+![Python](images/python.svg)
+![Golang](images/go.svg)
+![WebRTC](images/webrtc.svg)
+![Gstreamer](images/gstreamer.svg)  
+All hail the penguin!![Linux](images/tux.svg)  
+  
 
-- 🔭 I’m currently working on ...
-- 🌱 I’m currently learning ...
-- 👯 I’m looking to collaborate on ...
-- 🤔 I’m looking for help with ...
-- 💬 Ask me about ...
-- 📫 How to reach me: ...
-- 😄 Pronouns: ...
-- ⚡ Fun fact: ...
--->
+## Stats
+
+[![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=Xosrov&layout=compact&theme=dark)](https://github.com/anuraghazra/github-readme-stats)
---
+++ update_images.py
@@ -0,0 +1,179 @@
+from html2image import Html2Image
+from datetime import datetime
+import pathlib
+import requests
+import os
+
+CURRENT_PATH = pathlib.Path(__file__).parent.resolve()
+WEATHER_TOKEN = os.environ["WEATHER_API"]
+CITY = "Tehran"
+
+MONTHS = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec"
+]
+BASE_COLOR = "rgb(70,18,32)"
+ACCENT_COLOR = "rgb(199,62,29)"
+SUNNY_COLOR = "rgb(255,209,102)"
+RAINY_COLOR = "rgb(38,84,124)"
+SNOWY_OTHER_COLOR = "rgb(159,164,169)"
+
+# get time in UTC
+now = datetime.utcnow()
+day = now.day
+month = MONTHS[now.month-1]
+year = now.year
+
+# create HTML contents
+dateTimeHtml = f'''<html lang="en">
+<head>
+    <style>
+        * {{
+            font-family: 'Courier New', Courier, monospace;
+            font-size: 25px;
+        }}
+        body {{
+            display: contents;
+        }}
+        .container {{
+            margin: 3px 0 0 0;
+            box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 20px -1px, rgba(0, 0, 0, 0.3) 0px 1px 10px -1px;
+            background-color: {BASE_COLOR};
+            border-radius: 8px 0 0 8px;
+            display: flex;
+            width: max-content;
+            height: 30px;
+        }}
+        .container .left {{
+            background-color: {BASE_COLOR};
+            color: white;
+            border-radius: 8px 0 0 8px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            width: 60px;
+        }}
+        .container .right {{
+            box-shadow: -3px 0 6px -4px rgba(0, 0, 0, 0.75);
+            background-color: {ACCENT_COLOR};
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            border-radius: 8px 0 0 8px;
+            width: 170px;
+        }}
+        .container .right::after {{
+            content: 'UTC';
+            font-size: 8px;
+            font-style: italic;
+            margin-left: 2px;
+            margin-top: 12px;
+            color: black;
+        }}
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="left">
+            {day}
+        </div>
+        <div class="right">
+            {month} {year}
+        </div>
+    </div>
+</body>
+</html>'''
+
+# current today's weather status
+current_weather_stat = None
+
+# base URL for weather api (https://openweathermap.org/current)
+url = f"https://api.openweathermap.org/data/2.5/weather?q={CITY}&units=metric&appid={WEATHER_TOKEN}"
+# HTTP request
+response = requests.get(url)
+weatherData = response.json().get("weather", [{}])[0]
+weatherID, weatherType = weatherData.get("id", -1), weatherData.get("main", "")
+
+# https://openweathermap.org/weather-conditions
+if weatherID in range(200, 400) or weatherID in range(500, 600):
+    current_weather_stat = RAINY_COLOR
+elif weatherID in range(600, 800) or weatherID in range(801, 810):
+    current_weather_stat = SNOWY_OTHER_COLOR
+elif weatherID == 800:
+    current_weather_stat = SUNNY_COLOR
+
+# transition html
+transitionHtml = f'''
+<div style="position: absolute; top: 3px; left: 0px; width:300px; height:30px; box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 20px -1px, rgba(0, 0, 0, 0.3) 0px 1px 10px -1px; background: {ACCENT_COLOR}; background: linear-gradient(90deg, {ACCENT_COLOR} 0%, {current_weather_stat} 100%);">
+</div>
+'''
+
+weatherHtml = f'''<html lang="en">
+<head>
+    <style>
+        * {{
+            font-family: 'Courier New', Courier, monospace;
+            font-size: 25px;
+        }}
+        body {{
+            display: contents;
+        }}
+        .container {{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            padding-right: 10px;
+            border-radius: 0 10px 10px 0;
+            margin: 3px 0 0 0;
+            box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 20px -1px, rgba(0, 0, 0, 0.3) 0px 1px 10px -1px;
+            background-color: {current_weather_stat};
+            display: flex;
+            width: max-content;
+            height: 30px;
+            width: 210px;
+        }}
+        .container::after {{
+            content: '@{CITY}';
+            font-size: 7px;
+            font-style: italic;
+            margin-left: 2px;
+            margin-top: 12px;
+            color: {BASE_COLOR};
+        }}
+    </style>
+</head>
+<body>
+    <div class="container">
+        {weatherType}
+    </div>
+</body>
+</html>'''
+
+# generate images
+htmi = Html2Image()
+htmi._output_path = CURRENT_PATH.joinpath("images")
+htmi.screenshot(
+    size=(230, 36),
+    html_str=dateTimeHtml,
+    save_as="date.png",
+)
+htmi.screenshot(
+    size=(300, 36),
+    html_str=transitionHtml,
+    save_as="transition.png",
+)
+htmi.screenshot(
+    size=(225, 36),
+    html_str=weatherHtml,
+    save_as="weather.png",
+)
